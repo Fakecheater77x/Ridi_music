571a8b009c8ff6a265910e5ef4bc30774e9fe400
PROFESSOR <160086180+Fakecheater77x@users.noreply.github.com>
2024-09-09 12:26:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 
 # Get this value from @MissRose_Bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", None))
+OWNER_ID = int(getenv("OWNER_ID", 6169529415))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Adithakur008/Ridi_music",
+    "https://github.com/Fakecheater77x/RAJPUT-X-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+B1_6C_CnB0AxNWY1")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+hZtWhC590gFiODNl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FZ4EZONE4EVER")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+HzGQKfT01p4yYTE1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
